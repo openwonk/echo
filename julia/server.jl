4e981a792bf89fa5a96d6df47d3f2873744e3a91
openwonk <bryan@neoty.pe>
2015-02-01 19:33:47 -0800

    auto commit

--- julia/server.jl
+++ julia/server.jl
@@ -2,23 +2,29 @@ function +(a::String, b::String)
 	a * b
 end
 
-port = 8080
-server = listen(port)
-println("listen @ " + string(port))
+function Echo(port)
+	# port = 8080
+	server = listen(port)
+	println("listen @ " + string(port))
 
-while true
-	conn = accept(server)
-	@async begin
-		try
-			while true
-				incoming = readline(conn)
-				print("> ", incoming)
-				write(conn, incoming)
+	while true
+		conn = accept(server)
+		@async begin
+			try
+				while true
+					incoming = readline(conn)
+					print("> ", incoming)
+					write(conn, incoming)
+					# incoming = replace(input, "\n", "")
+					# if incoming == ":exit" || input == ":quit"
+				end
+			catch err
+				print("error: $err")
 			end
-		catch err
-			print("error: $err")
-		end
 
+		end
 	end
 end
+
+Echo(8080)
 
